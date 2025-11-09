8d7b5313a9500f9c1d02ee985c53e70d5f085c1c
mavericc <dj132004@gmail.com>
2025-11-09 18:03:56 +0530

    build metadata & local aggr.

--- dmcop.py
+++ dmcop.py
@@ -1,57 +1,78 @@
+from matplotlib.pylab import nbytes
 import torch
-import mpi4py.MPI as MPI
+from comms import RingCommunicator
 
-comm = MPI.COMM_WORLD     
-rank = comm.Get_rank()   
-size = comm.Get_size()
+PARAM_INFO = [] # metadata for parameters
+START = 0
+METADATA_AVAIL = False
+PARAMETERS_ASSIGNED = False
 
 class DMCOptimizer(torch.optim.Optimizer):
-    """A wrapper around the optimizer used by user to perform all-reduce on gradients before parameter update"""
+    """A wrapper around the optimizer used by user to perform all-reduce on gradients before parameter update. """
     def __init__(self, optimizer, grad_staleness):
         self.optimizer = optimizer
-        self.step_count = 0 
         self.grad_staleness = grad_staleness
+        self.step_count = 0
         super().__init__(optimizer.param_groups, defaults=getattr(optimizer, "defaults", {}))
+        params = []
+        for g in optimizer.param_groups:
+            params.extend(g['params'])
+        self.all_params = params # Store the collected parameters
+        self.comm = RingCommunicator()
 
-    def step(self):
+    def step(self) -> None:
+        global METADATA_AVAIL, START, PARAMETERS_ASSIGNED # Declare METADATA_AVAIL and START as global here
+
         self.step_count += 1
 
         for group in self.optimizer.param_groups:
             for p in group['params']:
-               if p.grad is None:
-                    continue
+                if METADATA_AVAIL == False:
+                    nbytes = p.numel() * p.element_size()
+                    PARAM_INFO.append({
+                        'shape': p.shape,
+                        'nbytes': nbytes,
+                        'start': START,
+                        'end': START + nbytes
+                    })
+                    START += nbytes
 
-               # Yay, every parameter gets its own temp buffer (accum_grad) to accumulate gradients
-               if not hasattr(p, 'accum_grad'):
-                    p.accum_grad = torch.zeros_like(p.grad)
-
-               # Accumulate gradients locally by adding
-               # corresponding gradients to their personal accum_grad buffer
-               p.accum_grad.add_(p.grad)
-               #print(f"Grad before reduction → {p.grad.shape}")
-
-        # Perform local gradient reduction to reduce network overhead. 
-        # Accuracy loss is imminent but can be controlled by grad_staleness parameter
-        if self.step_count % self.grad_staleness == 0:
-           for group in self.optimizer.param_groups:
-              for p in group['params']:
                 if p.grad is None:
                     continue
+
+                # Yay, every parameter gets its own temp buffer (accum_grad) to accumulate gradients
+                if not hasattr(p, 'accum_grad'):
+                    p.accum_grad = torch.zeros_like(p.grad)
+
+                # Accumulate gradients locally by adding
+                # corresponding gradients to their personal accum_grad buffer
+                p.accum_grad.add_(p.grad)
+                #print(f"Grad before reduction → {p.grad.shape}")
+            METADATA_AVAIL = True
+            if METADATA_AVAIL == True and PARAMETERS_ASSIGNED == False:
+                self.comm.assign_parameters(self.all_params, PARAM_INFO, total_bytes=START)
+                PARAMETERS_ASSIGNED = True
+
+        # Perform local gradient reduction to reduce network overhead.
+        # Accuracy loss is imminent but can be controlled by grad_staleness parameter
+        if self.step_count % self.grad_staleness == 0:
+            for group in self.optimizer.param_groups:
+                for p in group['params']:
+                    if p.grad is None:
+                        continue
 
                 p.grad.copy_(p.accum_grad / self.grad_staleness)
                 #print(f"Reduced (averaged) gradient → {p.grad.shape}")
                 # Reset accumulator for next accumulation of gradients
                 # for next cycle of grad_staleness steps
+
                 p.accum_grad.zero_()
 
-        
-        # Inter-node gradient all-reduce begins here
+            # Inter-node gradient all-reduce begins here
+            self.comm.ring_all_reduce(self.all_params, PARAM_INFO, total_bytes=START)
 
         self.optimizer.step()
         self.optimizer.zero_grad()
 
-    def zero_grad(self):
+    def zero_grad(self) -> None:
         self.optimizer.zero_grad()
-
-if __name__ == "__main__":
-    print(f"DMCOptimizer initialized on rank {rank} out of {size} processes.")
