156bfa05a697ef31ebd2858a67b50cc97560967f
Bhavika Dhami <bhavika02@gmail.com>
2026-03-11 15:49:05 +0530

    Add sharding test script

---
+++ test.py
@@ -0,0 +1,53 @@
+import os
+from dmc_sharding import (
+    load_folder_dataset,
+    shard_groups_to_archives
+)
+
+
+def main():
+    root_path = "cifar_sharding_dataset/train"
+
+    print("\n[TEST] Checking dataset path...")
+    print("Current working directory:", os.getcwd())
+    print("Dataset exists?", os.path.exists(root_path))
+
+    if not os.path.exists(root_path):
+        print("ERROR: CIFAR dataset not found.")
+        return
+
+    print("\n[TEST] Loading CIFAR folder dataset...")
+
+    groups = load_folder_dataset(
+        root_dir=root_path,
+        allowed_extensions=None
+    )
+
+    print(f"\n[TEST] Found {len(groups)} logical groups")
+
+    total_size = 0
+
+    for group in groups:
+        print(
+            f"  - Group '{group['group_id']}' "
+            f"contains {len(group['items'])} files, "
+            f"size={group['size']} bytes"
+        )
+        total_size += group["size"]
+
+    print(f"\n[TEST] Total dataset size: {total_size} bytes")
+
+    print("\n[TEST] Starting sharding process...")
+
+    shard_groups_to_archives(
+        groups=groups,
+        output_dir="cifarshards_output",
+        num_shards=5,          # You can change this
+        compression="zstd"
+    )
+
+    print("\n[TEST] CIFAR sharding completed successfully!")
+
+
+if __name__ == "__main__":
+    main()
